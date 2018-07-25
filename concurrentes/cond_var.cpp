3f3b41ce6c12a4256d913a63293a915e8796825e
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-07-24 23:04:46 -0300

    Correccion de monitores
    Ya funciona el programa de productor consumidor, el orden de las funciones lanzaban error. Aun falta descubrir porque termine varias veces la clase

--- concurrentes/cond_var.cpp
+++ concurrentes/cond_var.cpp
@@ -8,7 +8,31 @@ std::queue<int32_t> the_queue;
 std::mutex the_mutex;
 std::condition_variable the_cv;
 
-class Threadsito;
+void wait_and_pop(int32_t& data);
+void push(int32_t const& data);
+
+
+class Threadsito{
+    int32_t entero;
+ public:
+	 Threadsito (int32_t _entero){
+         this->entero = _entero;
+         std::cout << "Constructor de Threadsito...\n";
+    };
+	 void operator()(); // sobrecarga operador ()
+	 ~Threadsito() { std::cout << "Destructor de Threadsito...\n"; }; // destructor
+ };
+ 
+ void Threadsito::operator()()
+ {
+    int32_t num;
+	std::cout << "::Dentro del thread::" << std::endl;
+    for(int x=0;x< this->entero;x++){
+        wait_and_pop(num);
+        std::cout << "Cargando numero " << num << std::endl;
+    }
+ }
+ 
 
 void wait_and_pop(int32_t& data){
     std::unique_lock<std::mutex> lk(the_mutex);
@@ -24,9 +48,9 @@ void push(int32_t const& data){
 }
 
 int main(){
-    std::cout << "Numero de threas" << std::thread::hardware_concurrency() << std::endl;
+    std::cout << "Numero de threas:" << std::thread::hardware_concurrency() << std::endl;
     std::vector<int32_t> vec{1,2,3,4};
-    Threadsito th( 4);//(int32_t) vec.capacity());
+    Threadsito th(vec.capacity());
 
 	std::thread t(th);
     
@@ -35,29 +59,8 @@ int main(){
         push(ite);
     }
 
-
+    t.join();
+    //Si no se coloca dice que termino sin excepciones( aunque no halla terminado el thread)
 
     return 0;
 }
-
-class Threadsito{
-    int32_t entero;
- public:
-	 Threadsito (int32_t _entero){
-         this->entero = _entero;
-         std::cout << "Constructor de Threadsito...\n";
-    };
-	 void operator()(); // sobrecarga operador ()
-	 ~Threadsito() { std::cout << "Destructor de Threadsito...\n"; }; // destructor
- };
- 
- void Threadsito::operator()()
- {
-     int32_t num;
-	std::cout << "::Dentro del thread::" << std::endl;
-    for(int x=0;x< this->entero;x++){
-        wait_and_pop(num);
-        std::cout << "Cargando numero" << num << std::endl;
-    }
- }
- 
