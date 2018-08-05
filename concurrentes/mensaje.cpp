903afdda28be62ee1326d43476daa84b5928b527
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-08-04 23:20:53 -0300

    Mensajes con streams
    Prueba conceptual de canal de solo lectura.

--- concurrentes/mensaje.cpp
+++ concurrentes/mensaje.cpp
@@ -1,5 +1,6 @@
 #include <map>
 #include <iostream>
+#include <sstream>
 
 template<typename C>
 struct MessageExpression
@@ -19,14 +20,23 @@ struct SomeClass : public MessageExpression<SomeClass>
     void method2() { std::cout << "method2" << std::endl; };
 };
 
-int main()
-{
+int ejemplo1(){
     SomeClass s;
     s.registerFunc("method1",&SomeClass::method1);
     s.registerFunc("method2",&SomeClass::method2);
     s.sendMesg("method1");
     s.sendMesg("method2");
-    s.sendMesg("method3");
+    s.sendMesg("method3"); //No pasa nada
+    return 0;
+}
+
+
+int main()
+{
+    std::ostream os(&std::basic_streambuf());
+ 
+    onexit(ejemplo1);
+
 }
 
 //La unica forma de tener polimorfismo es por punteros void, o usar templates.
