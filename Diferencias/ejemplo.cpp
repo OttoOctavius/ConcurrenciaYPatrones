2dbef7dd273aa5d5ae38715204d0e19e5619d778
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-08-26 22:27:09 -0300

    Armando un ejemplo de async
    Describir cosas cotidianas en terminos computacionales.

--- Diferencias/ejemplo.cpp
+++ Diferencias/ejemplo.cpp
@@ -1,4 +1,37 @@
-#include <malloc.h>
+#include <stdlib>
+
+using namespace std;
+
+enum Distracciones = {telefonica,hermanos,padres};
+
+typedef void interrupcion(Distracciones quien,string porque);
+
+class Persona
+ {
+ public:
+	 Persona (); // constructor
+	 Persona(Persona &); // constructor de copia
+	 ~Persona(); // destructor
+
+	 estudiar(interrupcion i);
+	 comer(){ cout << "Comiendo" << endl;}
+	 descanzar(interrupcion i);
+ };
+ 
+ Persona::Persona()
+ {
+	 cout << "Constructor de Persona...\n";
+ }
+ 
+ Persona::Persona(Persona &)
+ {
+	 cout << "Constructor de copia de Persona...\n";
+ }
+ 
+ Persona::~Persona()
+ {
+	 cout << "Destructor de Persona...\n";
+ }
 
 int main(int argc, char const *argv[])
 {
