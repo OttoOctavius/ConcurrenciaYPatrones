04332ad50e64d32df89b256a79d576d5f95d1406
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-09-10 23:54:50 -0300

    Tratando de medir tiempo de una función cualquiera

--- concurrentes/tiempo.hpp
+++ concurrentes/tiempo.hpp
@@ -1,9 +1,12 @@
 #include<chrono>
 
+using namespace std;
+
 void medirtiempo(void &func()){
-auto start = std::chrono::system_clock::now();
-auto end = std::chrono::system_clock::now();
+    
+    auto start = std::chrono::system_clock::now();
     *fun();
+    auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end-start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
  
